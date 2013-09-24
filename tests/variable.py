23a93e7fa39869a44d92e36a9b619555382f3ffc
Jean-Philippe Braun <eon@patapon.info>
2013-09-24 20:14:26 +0200

    Add/Complete some tests

--- tests/variable.py
+++ tests/variable.py
@@ -1,48 +1,65 @@
 import unittest
-#import mss.common
-#mss.common.log_disable_stdout()
 
-from mss.variable import *
+from mss.variable import VList, VString, VInt
 
-v1=VString("v1",default="value1")
-v2=VString("v2",default="value2")
 
 class TestVariable(unittest.TestCase):
-    def test_VString(self):
-        t=VString("str1",default="default1")
-        self.assertEqual(t.value , "default1")
-        with self.assertRaises(TypeError):
-            t.value=1
-        t.value="default2"
-        self.assertEqual(t.value , "default2")
 
+    def test_VString(self):
+        t = VString("str1", default="default1")
+        self.assertEqual(t.value, "default1")
+        self.assertEqual(str(t), "default1")
+        with self.assertRaises(TypeError):
+            t.value = 1
+        with self.assertRaises(TypeError):
+            t.fill(1)
+        t.value = "default2"
+        self.assertEqual(t.value, "default2")
         t.fill("default3")
+        self.assertEqual(t.value, "default3")
+
+    def test_VInt(self):
+        t = VInt("int&", default=10)
+        self.assertEqual(t.value, 10)
+        self.assertEqual(int(t), 10)
+        with self.assertRaises(TypeError):
+            t.value = "foo"
+        with self.assertRaises(TypeError):
+            t.fill("foo")
+        t.value = 20
+        self.assertEqual(t.value, 20)
+        t.fill(30)
+        self.assertEqual(t.value, 30)
 
     def test_VList(self):
-        t=VList("list1",inner=VString)
+        t=VList("list1", inner=VString)
         # Type of a inner value is verified
         with self.assertRaises(TypeError):
-            t.value=[1]
+            t.value = [1]
+        with self.assertRaises(TypeError):
+            t.fill([1])
         # A correct data type can be set
-        t.value=["default1"]
-        self.assertEqual(t.value[0].value,"default1")
-        t.value[0].value="default2"
-        self.assertEqual(t.value[0].value,"default2")
-        
+        t.value = ["default1"]
+        self.assertEqual(t.value[0].value, "default1")
+        t.value[0].value = "default2"
+        self.assertEqual(t.value[0].value, "default2")
+        t.fill(["foo", "bar"])
+        self.assertEqual(t.value[0].value, "foo")
+        self.assertEqual(t.value[1].value, "bar")
+
     def test_VList_of_VList(self):
-        t=VList("Vlist",inner=VList("innerVList",inner=VString))
-        t.value=[["iop"]]
-        t.value[0].value=["v1","v2"]
-        print t.value[0].value
-        self.assertEqual(t.value[0].value,["v1","v1"])
-
-    def test_fill_VString(self):
-        t=VString("str1")
-        t.fill("iop")
-
-    def test_fill_VList(self):
-        t=VList("list1",inner=VString)
-        t.fill(["iop1","iop2"])
+        t = VList("Vlist", inner=VList("innerVList", inner=VString))
+        t.value = [["foo", "bar"], ["test", "str"]]
+        t.value[0].value = ["v1", "v2"]
+        self.assertEqual(t.value[0].value[0].value, "v1")
+        self.assertEqual(t.value[0].value[1].value, "v2")
+        self.assertEqual(t.value[1].value[0].value, "test")
+        self.assertEqual(t.value[1].value[1].value, "str")
+        t.fill([["str1"], ["str2", "str3"], ["str4"]])
+        self.assertEqual(t.value[0].value[0].value, "str1")
+        self.assertEqual(t.value[1].value[0].value, "str2")
+        self.assertEqual(t.value[1].value[1].value, "str3")
+        self.assertEqual(t.value[2].value[0].value, "str4")
 
     def test_required(self):
         t=VString("str")
@@ -57,5 +74,3 @@ class TestVariable(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-exit(1)
