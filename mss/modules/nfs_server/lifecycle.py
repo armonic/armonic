2642bcaba4dddf610d41629f3c869bf73915f6b7
Antoine Eiche <aeiche@mandriva.com>
2013-12-12 15:42:52 +0100

    First working version of nfs_server

--- mss/modules/nfs_server/lifecycle.py
+++ mss/modules/nfs_server/lifecycle.py
@@ -1,20 +1,79 @@
-from mss.lifecycle import State, Transition, Lifecycle, provide
+from mss.lifecycle import State, Transition, Lifecycle, provide, flags
 from mss.require import Require, RequireExternal
-from mss.variable import Port
+from mss.variable import Port, Host, VString
 import mss.state
+
+import mss.configuration_augeas as augeas
 
 import mss.common
 import logging
 logger=logging.getLogger(__name__)
 
+
+
+class Options(augeas.Nodes):
+    label = "option"
+    cls = augeas.Node
+
+class Client(augeas.Node):
+    options = Options
+
+class Clients(augeas.Nodes):
+    label = "client"
+    cls = Client
+
+class Dir(augeas.Node):
+    clients = Clients
+
+class Dirs(augeas.Nodes):
+    label = "dir"
+    baseXpath = "/files/etc/exports"
+    cls = Dir
+
+class Configuration(augeas.Configuration):
+    lenses={"Exports":["/etc/exports"]}
+    dirs = Dirs
+
+    def add_dir(self, directory, client_addr, options = []):
+        dir = Dir()
+        dir.value = directory
+        self.dirs.append(dir)
+
+        client = Client()
+        client.value = client_addr
+        dir.clients.append(client)
+        option = augeas.Node()
+        option.value = "ro"
+        client.options.append(option)
+
+    def rm_dir(self, directory, client):
+        """To remove the dir of a client. If several client use this dir, all
+        of them will be removed.
+        """
+        for d in self.dirs:
+            if client != None:
+                for c in d.clients:
+                    if c.value == client:
+                        d.rm()
+                        return
+
 class NotInstalled(State):pass
 class Installed(mss.state.InstallPackagesUrpm):
-    packages=["nfs-server"]
+    packages=["nfs-utils"]
 class Configured(State):
-    pass
+    
+    @Require("export", variables=[VString("dir"), Host("host")])
+    @flags({'restart':True})
+    def add_dir(self, requires):
+        conf = Configuration()
+        dir = requires.get("export").variables().get("dir").value
+        host = requires.get("export").variables().get("host").value
+    
+        conf.add_dir(dir, host, ["rw", "sync"])
+        conf.save()
 
 class Active(mss.state.ActiveWithSystemd):
-    services=["nfs-server"]
+    services=["nfs-common", "nfs-server"]
     
     @provide()
     def get_dir(self, requires):
