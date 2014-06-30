0d42e6d0f44f6b09bdd9a8fc896a6cb68b66aeca
lewo <aeiche@mandriva.com>
2014-06-30 20:26:46 +0200

    Move read_string in client.utils. This is now also used by smartonic.

--- armonic/client/utils.py
+++ armonic/client/utils.py
@@ -1,3 +1,13 @@
+def read_string(string):
+    """Try to transform string argument value to armonic primitive type"""
+    if string.startswith('[') and string.endswith(']'):
+        return string[1:-1].split(",")
+    try:
+        return int(string)
+    except ValueError:
+        return string
+
+
 def require_validation_error(dct):
     """Take the return dict of provide_call_validate and return a list of
     tuple that contains (xpath, error_string)"""
